1f4732fa8dcd4fc6d9229d5c824d7044a8f9b9b3
tuongnguyen <hientuong10101997@gmail.com>
2021-10-13 15:45:14 +0700

    first commit

---
+++ PPMToolFullStack/src/main/java/com/io/agileintelligence/ppmtool/PPMToolFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.io.agileintelligence.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PPMToolFullStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PPMToolFullStackApplication.class, args);
+    }
+
+}
---
+++ PPMToolFullStack/src/test/java/com/io/agileintelligence/ppmtool/PPMToolFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.agileintelligence.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PPMToolFullStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
